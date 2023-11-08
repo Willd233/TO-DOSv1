6c7d8a6f6616030836f1b03e0d4b91c25dcf3802
Willd233 <emiliod255@gmail.com>
2023-11-07 22:52:23 -0400

    reparado

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,18 +1,43 @@
 import './style/TodoItem.css';
 
 function TodoItem(props) {
-    return(
-        <li className="TodoItem">
-          <span className={`Icon Icon-check 
-          ${props.completed && "Icon-check--active"}`}>
-            V</span>
-          <p className={`TodoItem-p ${props.
-            completed && "TodoItem-p--complete"}`}>
-          {props.text}
-          </p>
-          <span className="Icon Icon-delete">
-            X</span>  
-        </li>
-    );
-  }
-export { TodoItem };  
+  return (
+    <li className="TodoItem">
+      <span
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={props.onComplete}
+      >
+        V
+      </span>
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
+      </p>
+      <span
+        className="Icon Icon-delete"
+        onClick={props.onDelete}
+      >
+        X
+      </span>
+    </li>
+  );
+}
+
+export { TodoItem };
+// 
+
+// function TodoItem(props) {
+//     return(
+//         <li className="TodoItem">
+//           <span className={`Icon Icon-check 
+//           ${props.completed && "Icon-check--active"}`}>
+//             V</span>
+//           <p className={`TodoItem-p ${props.
+//             completed && "TodoItem-p--complete"}`}>
+//           {props.text}
+//           </p>
+//           <span className="Icon Icon-delete">
+//             X</span>  
+//         </li>
+//     );
+//   }
+// export { TodoItem };  
