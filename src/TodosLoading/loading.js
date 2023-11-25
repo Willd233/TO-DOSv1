b23429af12869d524cc8ff9cf177b0a6c537402b
Willd233 <emiliod255@gmail.com>
2023-11-25 15:25:14 -0400

    styles for content

--- src/TodosLoading/loading.js
+++ src/TodosLoading/loading.js
@@ -5,17 +5,14 @@ function TodosLoading(){
         <div className="item-container-skeleton">
             <div className="img-round skeleton"></div>
             <p className="skeleton skeleton-text__body skeleton-text"></p>
-            <div className="img-round skeleton"></div>
         </div>
          <div className="item-container-skeleton">
             <div className="img-round skeleton"></div>
             <p className="skeleton skeleton-text__body skeleton-text"></p>
-            <div className="img-round skeleton"></div>
          </div>
          <div className="item-container-skeleton">
             <div className="img-round skeleton"></div>
             <p className="skeleton skeleton-text__body skeleton-text"></p>
-            <div className="img-round skeleton"></div>
          </div>
          </>
     );
